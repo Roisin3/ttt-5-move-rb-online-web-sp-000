2abf9142c15ab9c4b855704075044b663daf9935
Michael Lynch <michael@michaelrlynch.com>
2019-07-28 06:06:28 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,4 @@
+# Display for board state
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -7,10 +8,14 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+# Convert user input to string and subtract 1 for ... computer counting (?)
 def input_to_index(user_input)
   user_input.to_i - 1
 end
 
+# Takes arguments board, #input_to_index, and user_choice(with default value of X)
+# assigns board array the return value of input_to_index and sets it to string X
+# 
 def move(board, input_to_index, user_choice = "X")
   board[input_to_index] = user_choice
 end
