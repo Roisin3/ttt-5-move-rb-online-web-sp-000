e40f963bb6f9f9ab3c9bc17a2a6385b469f4f803
Michael Lynch <michael@michaelrlynch.com>
2019-07-28 06:08:55 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -14,8 +14,9 @@ def input_to_index(user_input)
 end
 
 # Takes arguments board, #input_to_index, and user_choice(with default value of X)
-# assigns board array the return value of input_to_index and sets it to string X
-#
+# assigns board array the return value of input_to_index and sets it to string X.
+# This is where I am getting lost. I wrote it and know how it *SHOULD* work but
+# cannot figure out *HOW* it is working. 
 def move(board, input_to_index, user_choice = "X")
   board[input_to_index] = user_choice
 end
